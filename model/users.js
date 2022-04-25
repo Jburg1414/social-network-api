05b30b75c91731eb6137ad1c71fe1b3ea5b78766
Jburg1414 <Jburgess1414@gmail.com>
2022-04-24 18:37:40 -0700

    initial set up of project

---
+++ model/users.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trimmed: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      // grab email regex from module 17
+      match: [, "Not a valid email!"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thoughts",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Users",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const Users = model("Users", userSchema);
+
+module.exports = Users;
